40ffd373c020acdc0da085979a287fba1c8b3569
iury santos <iurysantos654@gmail.com>
2026-01-10 18:42:12 -0300

    chore: viewset

---
+++ hello.py
@@ -0,0 +1,8 @@
+nome ="iury"
+idade=21
+altura=1.75
+peso=76
+
+imc = peso / (altura * altura)  
+
+print("Olá meu nome é "+ str(nome) + " e eu tenho " + str(idade) + " anos e meu imc é de " + str(imc) )
