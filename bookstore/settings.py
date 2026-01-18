7952a407193a5ffeac30df26f142e7a96b3425ea
iury santos <iurysantos654@gmail.com>
2026-01-18 19:06:14 -0300

    Chore: network

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-9)x93wfm)tmfg7vm=*s*wx_&(^m&7+0dsf&@ovqid*37jy=#=w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS configured via environment variable at end of file
 
 
 # Application definition
