03817a88a34174515c3ae1d08ca30fb1b803c480
iury santos <iurysantos654@gmail.com>
2026-02-12 19:23:49 -0300

    Atualizar configurações do Django

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -157,4 +157,4 @@ REST_FRAMEWORK = {
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-test-key-for-ci")
 DEBUG = int(os.environ.get("DEBUG", default=0))
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]").split(",")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ebac-bookstore-api.herokuapp.com', 'josedev.pythonanywhere.com']
